3d899e0c8a4fa6c2a96d520b8d0462f8d8f3c60b
Prozhikin_Andrey <a.prozhikin@gmail.com>
2022-11-30 21:03:32 +0200

    Feat: Add task of homework3

---
+++ Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.Write("Введите X точки А:");
+int xa = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y точки А:");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Z точки А:");
+int za = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите X точки B:");
+int xb = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y точки B:");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Z точки B:");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+// √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+double result = Math.Sqrt(Math.Pow(xb-xa,2)+Math.Pow(yb-ya,2)+Math.Pow(zb-za,2));
+Console.WriteLine (Math.Round(result,2,MidpointRounding.ToZero));
---
+++ Task23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход 2 числа (N,M) и выдаёт таблицу кубов чисел от N до M.
+// 2,3 -> 8, 27
+// 1,5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count=n;
+if (count>m) Console.Write("Введенное число N больше числа M");
+else 
+{
+while (count<m)
+{
+  Console.Write ($"{Math.Pow(count,3)}, ");
+  count++;  
+}
+Console.Write ($"{Math.Pow(count,3)}");
+}
